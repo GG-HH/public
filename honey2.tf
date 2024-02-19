11b67bc3f357fe6f3d23b8cb0d308bd36a57c201
Henri Hubert <henri.hubert@gitguardian.com>
2024-02-19 19:15:36 +0100

    chore: copy

---
+++ honey2.tf
@@ -0,0 +1,6 @@
+aws_access_key_id = AKIAXITLBWRMQTJR5QH5
+aws_secret_access_key = 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
+
+
+aws_access_key_id = AKIAXITLBWRMQTJR5QH5
+aws_secret_access_key = 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
